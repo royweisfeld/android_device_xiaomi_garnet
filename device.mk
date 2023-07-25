b5ad00b54916205a2dddd28f0a9d7aed77c4257c
bibindasb <bibindasappu1@gmail.com>
2023-07-25 20:06:52 +0530

    garnet: thermal: Switch to QTI thermal
    *using custom thermal config

--- device.mk
+++ device.mk
@@ -547,7 +547,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0.vendor
+    android.hardware.thermal@2.0-service.qti
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
