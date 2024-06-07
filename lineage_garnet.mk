42cdfc9459fc38c1362851f70f862bc61cfe532e
Roy Weisfeld <roy.weisfeld+git@hotmail.com>
2024-06-07 15:08:33 +0300

    Matrixx: bringup

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -8,8 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CrDroid stuff.
-TARGET_DISABLE_EPPE := true
+# Inherit some common stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from garnet device
@@ -18,27 +17,43 @@ $(call inherit-product, device/xiaomi/garnet/device.mk)
 # MiuiCamera
 $(call inherit-product-if-exists, vendor/xiaomi/garnet-miuicamera/products/miuicamera.mk)
 
-# CrDroid stuff
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_HAS_UDFPS := true
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_EXCLUDES_AUDIOFX := true
-
 PRODUCT_NAME := lineage_garnet
 PRODUCT_DEVICE := garnet
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 2312DRA50G
-
 PRODUCT_SYSTEM_NAME := garnet_global
 PRODUCT_SYSTEM_DEVICE := garnet
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="garnet_global-user 14 UKQ1.231003.002 V816.0.9.0.UNRMIXM release-keys" \
+    PRIVATE_BUILD_DESC="garnet_global-user 14 UKQ1.231003.002 V816.0.6.0.UNRMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := Redmi/garnet_global/garnet:14/UKQ1.231003.002/V816.0.9.0.UNRMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/garnet_global/garnet:14/UKQ1.231003.002/V816.0.6.0.UNRMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Set res to remove warning
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# UDFPS Flags
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+
+# Extra Flags
+TARGET_DISABLE_EPPE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Matrixx flags
+MATRIXX_BUILD_TYPE := Official
+MATRIXX_CHIPSET := SM7435-AB
+MATRIXX_BATTERY := 5100 mAh
+MATRIXX_DISPLAY := 1220x2712
+MATRIXX_MAINTAINER := GameNCode
+
+# Matrixx GMS
+WITH_GMS := true
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
